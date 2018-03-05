89e13c6f6373df973667d24c9e641c1f98117e36
TDhananjaya <tharindudhananjaya1122@gmail.com>
2018-03-05 13:24:45 +0530

    adding game Assests

---
+++ Assets/Scripts/HalfPointTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class HalfPointTrigger : MonoBehaviour {
+
+	public GameObject LapCompleteTrigger;
+	public GameObject HalfLapTrigger;
+	
+
+	void OnTriggerEnter () {
+		LapCompleteTrigger.SetActive (true);
+		HalfLapTrigger.SetActive (false);
+	}
+}
